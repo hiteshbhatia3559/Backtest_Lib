922f083d479f42e9f27ec7ad85d23b3578307d74
irage <hiteshbhatia3559@gmail.com>
2019-04-16 18:32:46 +0530

    Perf metrics

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import os
 
 bid, ask = resample('1 (1).log', '1Min')
 
-rsi_windows = range(5, 50)  # 45
-rsi_oversold_bounds = range(10, 50)  # 40
-rsi_overbought_bounds = range(50, 90)  # 40
-ema_values = range(10, 100)  # 90
-targets = range(100, 2000, 100)  # 19
-stops = range(100, 2000, 100)  # 19
+rsi_windows = range(14, 15)  # 45
+rsi_oversold_bounds = range(30, 31)  # 40
+rsi_overbought_bounds = range(70, 71)  # 40
+ema_values = [14,28]# range(14, 100)  # 90
+targets = [200,300,400,500]# range(100, 2000, 100)  # 19
+stops = [200,300]  # 19
 overlaps = [True, False]  # 2
 i = 0
 results = []
@@ -25,114 +25,114 @@ for overlap in overlaps:
                     for fast_ema in ema_values:
                         for target in targets:
                             for stop in stops:
-                                try:
 
-                                    settings = "overlap_{}-rsiwindow_{}-rsiupper_{}-rsilower_{}-slowema_{}-fastema_{}-target_{}-stop_{}".format(
-                                        overlap, rsi_window, rsi_upper, rsi_lower, slow_ema, fast_ema, target, stop)
-                                    # print(settings)
-                                    ask['RSI'] = talib.RSI(ask['close'], timeperiod=rsi_window)
-                                    ask['MA_fast'] = talib.EMA(ask['close'], timeperiod=fast_ema)
-                                    ask['MA_slow'] = talib.EMA(ask['close'], timeperiod=slow_ema)
+                                settings = "overlap_{}-rsiwindow_{}-rsiupper_{}-rsilower_{}-slowema_{}-fastema_{}-target_{}-stop_{}".format(
+                                    overlap, rsi_window, rsi_upper, rsi_lower, slow_ema, fast_ema, target, stop)
+                                # print(settings)
+                                ask['RSI'] = talib.RSI(ask['close'], timeperiod=rsi_window)
+                                ask['MA_fast'] = talib.EMA(ask['close'], timeperiod=fast_ema)
+                                ask['MA_slow'] = talib.EMA(ask['close'], timeperiod=slow_ema)
 
-                                    longs = []
-                                    shorts = []
+                                longs = []
+                                shorts = []
 
-                                    # STRATEGY
-                                    # Return type is a dict with
-                                    # if valid : {timestamp of entry, timestamp of exit, entry price, target price, stop price, type of exit, pnl}
-                                    # if invalid : {timestamp_of_entry, type_of_exit}
-                                    # print(row[1]['RSI']) # RSI VALUES HERE, row[0] is index (timestamp)
+                                # STRATEGY
+                                # Return type is a dict with
+                                # if valid : {timestamp of entry, timestamp of exit, entry price, target price, stop price, type of exit, pnl}
+                                # if invalid : {timestamp_of_entry, type_of_exit}
+                                # print(row[1]['RSI']) # RSI VALUES HERE, row[0] is index (timestamp)
 
-                                    for row in ask.iterrows():
-                                        if row[1]['MA_fast'] > row[1]['MA_slow']:
-                                            if row[1]['RSI'] < rsi_lower:  # Oversold condition
-                                                longs.append(
-                                                    make_long(longs, bid, row, lots=10, target=target, stop=stop,
-                                                              overlap=overlap))
-                                        if row[1]['MA_fast'] < row[1]['MA_slow']:
-                                            if row[1]['RSI'] > rsi_upper:  # Overbought condition
-                                                shorts.append(
-                                                    make_short(shorts, ask, row, lots=10, target=stop, stop=stop,
-                                                               overlap=overlap))
-                                    # STRATEGY
+                                for row in ask.iterrows():
+                                    if row[1]['MA_fast'] > row[1]['MA_slow']:
+                                        if row[1]['RSI'] < rsi_lower:  # Oversold condition
+                                            longs.append(
+                                                make_long(longs, bid, row, lots=10, target=target, stop=stop,
+                                                          overlap=overlap))
+                                    if row[1]['MA_fast'] < row[1]['MA_slow']:
+                                        if row[1]['RSI'] > rsi_upper:  # Overbought condition
+                                            shorts.append(
+                                                make_short(shorts, ask, row, lots=10, target=stop, stop=stop,
+                                                           overlap=overlap))
+                                num_longs, num_shorts = 0, 0
+                                for item in longs:
+                                    if item["type_of_exit"] in ["Win", "Loss"]:
+                                        num_longs += 1
+                                for item in shorts:
+                                    if item["type_of_exit"] in ["Win", "Loss"]:
+                                        num_shorts += 1
 
-                                    # PNL Calc
-                                    # PNL for longs
+                                if num_shorts == 0:
+                                    break
+                                if num_longs == 0:
+                                    break
+                                # STRATEGY
 
-                                    longs_pnl = 0
-                                    long_turnover = 0
-                                    for item in longs:
-                                        try:
-                                            longs_pnl += item["pnl"]
-                                            long_turnover += item["entry_price"] * 10
-                                        except:
-                                            pass
-                                    long_brokerage = long_turnover / 1000000000 * 838
-                                    net_long_pnl = longs_pnl - long_brokerage
+                                # PNL Calc
+                                # PNL for longs
 
-                                    # PNL for shorts
-                                    shorts_pnl = 0
-                                    short_turnover = 0
+                                longs_pnl = 0
+                                long_turnover = 0
+                                for item in longs:
+                                    try:
+                                        longs_pnl += item["pnl"]
+                                        long_turnover += item["entry_price"] * 10
+                                    except:
+                                        pass
+                                long_brokerage = long_turnover / 1000000000 * 838
+                                net_long_pnl = longs_pnl - long_brokerage
 
-                                    for item in shorts:
-                                        try:
-                                            shorts_pnl += item['pnl']
-                                            short_turnover += item["entry_price"] * 10
-                                        except:
-                                            pass
+                                # PNL for shorts
+                                shorts_pnl = 0
+                                short_turnover = 0
 
-                                    short_brokerage = short_turnover / 1000000000 * 838
-                                    net_short_pnl = shorts_pnl - short_brokerage
-                                    # print("Net PNL long is: " + str(net_long_pnl) + " and Net PNL short is: " + str(net_short_pnl))
+                                for item in shorts:
+                                    try:
+                                        shorts_pnl += item['pnl']
+                                        short_turnover += item["entry_price"] * 10
+                                    except:
+                                        pass
 
-                                    num_longs, num_shorts = 0, 0
-                                    for item in longs:
-                                        if item["type_of_exit"] in ["Win", "Loss"]:
-                                            num_longs += 1
-                                    for item in shorts:
-                                        if item["type_of_exit"] in ["Win", "Loss"]:
-                                            num_shorts += 1
+                                short_brokerage = short_turnover / 1000000000 * 838
+                                net_short_pnl = shorts_pnl - short_brokerage
+                                # print("Net PNL long is: " + str(net_long_pnl) + " and Net PNL short is: " + str(net_short_pnl))
 
-                                    profitability_longs = 0
-                                    profitability_shorts = 0
-                                    profitability_total = 0
+                                profitability_longs = 0
+                                profitability_shorts = 0
+                                profitability_total = 0
 
-                                    long_win, long_loss, short_win, short_loss = 0, 0, 0, 0
-                                    for item in longs:
-                                        if item["type_of_exit"] == "Win":
-                                            long_win += 1
-                                        if item["type_of_exit"] == "Win":
-                                            long_loss += 1
+                                long_win, long_loss, short_win, short_loss = 0, 0, 0, 0
+                                for item in longs:
+                                    if item["type_of_exit"] == "Win":
+                                        long_win += 1
+                                    if item["type_of_exit"] == "Win":
+                                        long_loss += 1
 
-                                    for item in shorts:
-                                        if item["type_of_exit"] == "Win":
-                                            short_win += 1
-                                        if item["type_of_exit"] == "Win":
-                                            short_loss += 1
+                                for item in shorts:
+                                    if item["type_of_exit"] == "Win":
+                                        short_win += 1
+                                    if item["type_of_exit"] == "Win":
+                                        short_loss += 1
 
-                                    profitability_longs = long_win / num_longs
-                                    profitability_shorts = short_win / num_shorts
-                                    profitability_total = (long_win + short_win) / (num_longs + num_shorts)
+                                profitability_longs = long_win / num_longs
+                                profitability_shorts = short_win / num_shorts
+                                profitability_total = (long_win + short_win) / (num_longs + num_shorts)
 
-                                    # Logic to return data as a result
-                                    if net_long_pnl != 0.0:
-                                        if net_short_pnl != 0.0:
-                                            results.append({"settings": settings,
-                                                            "netlongpnl": net_long_pnl,
-                                                            "netshortpnl": net_short_pnl,
-                                                            "netpnl": net_short_pnl + net_long_pnl,
-                                                            "profitability_longs": profitability_longs,
-                                                            "profitability_shorts": profitability_shorts,
-                                                            "profitability_total": profitability_total,
+                                # Logic to return data as a result
+                                if net_long_pnl != 0.0:
+                                    if net_short_pnl != 0.0:
+                                        results.append({"settings": settings,
+                                                        "netlongpnl": net_long_pnl,
+                                                        "netshortpnl": net_short_pnl,
+                                                        "netpnl": net_short_pnl + net_long_pnl,
+                                                        "profitability_longs": profitability_longs,
+                                                        "profitability_shorts": profitability_shorts,
+                                                        "profitability_total": profitability_total,
 
-                                                            })
-                                    i += 1
-                                    os.system('cls')
-                                    print(str(i) + " : " + profitability_total)
-                                except:
-                                    pass
+                                                        })
+                                i += 1
+                                print(str(i) + " : " + str(profitability_total)+" : "+str(net_short_pnl + net_long_pnl))
 
-with open("Results.csv", newline="") as outfile:
+with open("Results.csv","wb+", newline="") as outfile:
     writer = csv.writer(outfile)
     writer.writerows(list(results[0].keys()))
     for result in results:
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# Moved to main
-rsi_values = range(5,50)
-ema_values = range(10,100)
-print(rsi_values[0])
+import os
+
+print("a")
+os.system('cls')
