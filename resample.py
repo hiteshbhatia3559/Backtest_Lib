bcb9017182bbc8b178f3e17cbabe4f0e218dfddf
irage <hiteshbhatia3559@gmail.com>
2019-04-16 13:22:25 +0530

    Progress

--- resample.py
+++ resample.py
@@ -15,29 +15,43 @@ def resample(filename, timeframe='15min'):
             df.drop(["Remove.{}".format(str(x))], axis=1, inplace=True)
         except:
             pass
-
+    #print(df['Timestamp'])
+    #df.drop(df.index[50], inplace=True)
+    #df.drop(df.index[-50], inplace=True)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp']*1e9).values + pd.to_timedelta(5.5, unit='h')
+    #print(df['Timestamp'])
     df.set_index(["Timestamp"], inplace=True)
-    df.drop(df.index[50], inplace=True)
-    df.drop(df.index[-50], inplace=True)
 
-    df.index = pd.to_datetime(df.index, unit='ms')
+    #df.index = pd.to_datetime(df.index, unit='m')
+
     df[["B", "A"]] = df[["B", "A"]].astype(int)
     df = df.resample(timeframe).ohlc()
 
+    #print(df)
     return df["B"], df["A"]
 
 
 if __name__ == "__main__":
-    x, y = resample('1 (1).log', '100ms')
+    x, y = resample('1 (1).log', '15Min')
+
+
+
+    x.to_csv('new.csv')
 
     x['RSI'] = talib.RSI(x['open'], timeperiod=36)
     x['MA14'] = talib.EMA(x['close'], timeperiod=36)
     x['MA28'] = talib.EMA(x['close'], timeperiod=72)
-    f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    # f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
-    ax1.plot(x['close'].tail(100))
-    ax1.plot(y['close'].tail(100))
+    #plt.plot(x['high'].tail(600))
+    #plt.plot(x['low'].tail(600))
+    print(x['open'].std())
+    print(x['open'].mean())
+    print(x['open'].head())
 
+
+
+    # ax1.plot(y['close'])
     # ax1.plot(x['MA14'])
     # ax1.plot(x['MA28'])
     # ax2.plot(x['RSI'])
