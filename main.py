8511722a2b115adebd962d8e6df7c769f5790bc8
irage <hiteshbhatia3559@gmail.com>
2019-04-23 14:36:08 +0530

    Progress

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ if __name__ == "__main__":
 
     file_list = get_list_of_files('./', '.log')
 
+    for file in file_list:
+        parts = file.split('.')
+        if int(parts[-2]) < 20190123:
+            file_list.remove(file)
+
     pool = multiprocessing.Pool(2)
     output = pool.map(get_default_backtest, file_list)
 
